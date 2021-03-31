4975932a6e00c135a016f58638211b2a5db5b5bd
mistok <Mistreated@ukr.net>
2021-03-31 11:50:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useEffect, useState} from "react";
+import {interval, Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+
+
+
 
 function App() {
+
+   let [isRunning, setIsRunning] = useState(false)
+
+   let [currentTimer, setCurrentTimer] = useState(0)
+
+
+    useEffect(() => {
+        const unsubscribe$ = new Subject();
+        interval(300).pipe(
+                takeUntil(unsubscribe$)
+        ).subscribe(
+            () =>
+                isRunning
+                    ? setCurrentTimer(val => val + 1000) //time in milliseconds
+                    : null
+
+        )
+        return () => {
+            unsubscribe$.next();
+            unsubscribe$.complete();
+        }
+    }, [isRunning])
+    let startHandler = () => {
+        setIsRunning(true)
+
+    }
+    let pauseHandler = () => {
+       setIsRunning(false)
+    }
+    let stopHandler = () => {
+       setIsRunning(false)
+       setCurrentTimer(0)
+    }
+
+    let resetHandler = () => {
+       setCurrentTimer(0)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        testing task timer
       </header>
+        <div className="time_desk">
+
+            <span>
+                { new Date(currentTimer).toISOString().slice(11, 19) }
+            </span>
+        </div>
+
+        <div>
+            {
+                isRunning
+                    ? <button className="btn btn-primary " onClick={pauseHandler}>pause</button>
+                    : <button className="btn btn-primary " onClick={startHandler}>start</button>
+            }
+            <button className="btn btn-primary" onClick={stopHandler}>stop</button>
+            <button className="btn btn-primary" onClick={resetHandler}>reset</button>
+        </div>
     </div>
   );
 }
